51855d426c4a4807fcef1bbf9d84a9af22761926
CoKoLe <pzxsheng@163.com>
2014-09-29 16:52:27 +0800

    新增zip操作

--- src/com/util/ZipUtil.java
+++ src/com/util/ZipUtil.java
@@ -7,8 +7,12 @@ import org.apache.tools.ant.taskdefs.Expand;
 import org.apache.tools.ant.taskdefs.Zip;
 import org.apache.tools.ant.types.FileSet;
 import org.apache.tools.ant.types.PatternSet;
+import org.apache.tools.zip.ZipEntry;
+import org.apache.tools.zip.ZipFile;
+import org.apache.tools.zip.ZipOutputStream;
 
-import java.io.File;
+import java.io.*;
+import java.util.Enumeration;
 
 /**
  * Created by pengsheng on 2014/09/29.
@@ -16,11 +20,11 @@ import java.io.File;
 public class ZipUtil {
 
     private static final Log logger = LogFactory.getLog(ZipUtil.class);
-
+    private static final int BUFFER = 1024;
     /**
      * 解压缩
-     * @param sourceZip 源zip文件
-     * @param targetDir 生成的目标目录下
+     * @param sourceZip ： 源zip文件
+     * @param targetDir ：目标文件夹
      */
     public static void unZip(String sourceZip, String targetDir) {
         unZip(sourceZip, targetDir, "", "");
@@ -28,8 +32,8 @@ public class ZipUtil {
 
     /**
      * 解压缩
-     * @param sourceZip 源zip文件
-     * @param targetDir 生成的目标目录下
+     * @param sourceZip ：源zip文件
+     * @param targetDir ：目标文件夹
      * @param includePattern : 只解压缩包含的文件或文件夹,
      * @param excludePattern : 不解压缩包含的文件或文件夹
      * pattern是一个使用“,”或空格分隔的匹配字符串，其中 “**”代表所有文件或目录，“*.*”代表说有文件， “*.java”代表所有扩展名为java的文件
@@ -93,5 +97,97 @@ public class ZipUtil {
 
         zip.addFileset(fileSet);
         zip.execute();
+    }
+
+    /**
+     *  压缩文件包含路径
+     * @param sourceFile : 压缩的源文件夹
+     * @param targetZip : 生成的目标文件
+     * @throws Exception
+     */
+    public static void ziphasDir(String sourceFile, String targetZip) throws Exception {
+
+        File inputFile = new File(sourceFile);
+        File outputFile = new File(targetZip);
+
+        if (!outputFile.exists()) {
+            outputFile.createNewFile();
+        }
+
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(outputFile));
+        out.setEncoding("GBK");
+        zip(out, inputFile, "");
+        out.close();
+    }
+
+    private static void zip(ZipOutputStream out, File file, String base) throws Exception {
+
+        if (file.isDirectory()) {
+
+            File[] fileList = file.listFiles();
+            if (base.length() > 0) {
+                out.putNextEntry(new ZipEntry(base + "/"));
+            }
+
+            base = base.length() == 0 ? "" : base + "/";
+            for (int i = 0; i < fileList.length; i++) {
+                zip(out, fileList[i], base + fileList[i].getName());
+            }
+
+        } else {
+
+            out.putNextEntry(new ZipEntry(base));
+            FileInputStream in = new FileInputStream(file);
+            int len;
+            byte[] buf = new byte[BUFFER];
+            while ((len = in.read(buf, 0, BUFFER)) != -1) {
+                out.write(buf, 0, len);
+            }
+            in.close();
+            out.closeEntry();
+        }
+    }
+
+    /**
+     * 解压缩zip文件包含文件夹
+     * @param zipFileName : 源zip文件
+     * @param targetDir : 目标文件夹
+     * @throws Exception
+     */
+    public static void unZiphasDir(String zipFileName, String targetDir) throws Exception {
+
+        ZipFile zipFile = new ZipFile(zipFileName, "GBK");
+        Enumeration emu = zipFile.getEntries();
+        while (emu.hasMoreElements()) {
+            ZipEntry entry = (ZipEntry) emu.nextElement();
+            if (entry.isDirectory()) {
+                File dir = new File(targetDir + entry.getName());
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
+                continue;
+            }
+
+            BufferedInputStream bis = new BufferedInputStream(zipFile.getInputStream(entry));
+            File file = new File(targetDir + entry.getName());
+            File parent = file.getParentFile();
+            if (parent != null && (!parent.exists())) {
+                parent.mkdirs();
+            }
+
+            FileOutputStream fos = new FileOutputStream(file);
+            BufferedOutputStream bos = new BufferedOutputStream(fos, BUFFER);
+            byte[] buf = new byte[BUFFER];
+            int len = 0;
+            while ((len = bis.read(buf, 0, BUFFER)) != -1) {
+                fos.write(buf, 0, len);
+            }
+
+            bos.flush();
+            bos.close();
+            bis.close();
+        }
+
+        zipFile.close();
     }
 }
